14a9774eb904fe37172bb57315c714f17121182d
Adam Stokes <adam.stokes@ubuntu.com>
2015-02-07 15:41:47 -0500

    update static site
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ images/photogallery/photo32567/_vti_cnf/sldshow.js
@@ -0,0 +1,14 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|05 May 2004 22:54:41 -0000
+vti_syncwith_localhost\\e\:\\my work documents\\my webs\\backup_dec_5/e\:/my work documents/my webs/backup_dec_5:TW|03 Dec 2003 21:06:20 -0000
+vti_author:SR|www.ide-nc.com
+vti_modifiedby:SR|www.ide-nc.com
+vti_nexttolasttimemodified:TW|05 May 2004 22:54:41 -0000
+vti_timecreated:TR|06 Dec 2003 01:58:02 -0000
+vti_extenderversion:SR|5.0.2.2623
+vti_syncwith_localhost\\e\:\\my work documents\\my webs\\ide6/e\:/my work documents/my webs/ide6:TW|10 Dec 2003 03:29:24 -0000
+vti_backlinkinfo:VX|photogallery/photo32567/real.htm
+vti_syncwith_localhost\\e\:\\my work documents\\my webs\\ide8/e\:/my work documents/my webs/ide8:TW|10 Dec 2003 03:29:24 -0000
+vti_syncwith_localhost\\e\:\\my work documents\\my webs\\ide9/e\:/my work documents/my webs/ide9:TW|05 May 2004 22:54:41 -0000
+vti_cacheddtm:TX|06 Dec 2003 01:58:02 -0000
+vti_filesize:IR|4362
